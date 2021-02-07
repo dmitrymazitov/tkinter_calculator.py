2432b35861e4e02db4f7ea346e583889912f35d4
Dee_Mann.py <65096543+dmitrymazitov@users.noreply.github.com>
2021-02-07 16:48:03 +0300

    Update and rename calculator.py to calculator_v0.02.py

--- calculator_v0.02.py
+++ calculator_v0.02.py
@@ -21,30 +21,30 @@ class Calculator:
 		for r in range(1, 5):
 			for col in range(3):
 				self.digit_buttons.append(tk.Button(self.root, text=f'{self.digit}', bg='grey', bd=2, font=('Arial', 15), command=lambda x=self.digit: self.click_button(x-1)))
-				self.digit_buttons[self.digit-1].grid(row=r, column=col, stick='wens', padx=5, pady=5)
+				self.digit_buttons[self.digit-1].grid(row=r, column=col, stick='wens', padx=1, pady=1)
 				#заменяем "десятку" на "ноль" и завершаем цикл 
 				if self.digit_buttons[self.digit-1]['text'] == '10':
 					self.digit_buttons[self.digit-1]['text'] = '0'
-					self.digit_buttons[self.digit-1].grid(row=4, column=1, stick='wens')
+					self.digit_buttons[self.digit-1].grid(row=4, column=1, stick='wens', padx=1, pady=1)
 					break
 				self.digit+=1
 	
 		#создание кнопки "очистить"
 		self.clean_button = tk.Button(self.root, text='C', font=('Arial', 15), command=self.clean_win)
-		self.clean_button.grid(row=4, column=2, stick='wens')
+		self.clean_button.grid(row=4, column=2, stick='wens', padx=1, pady=1)
 		
 		#создание кнопки "отмена/назад"
-		self.back_button = tk.Button(self.root, text='#', font=('Arial', 15), command=self.back_mean)
-		self.back_button.grid(row=4, column=0, stick='wens')
-		
+		self.back_button = tk.Button(self.root, text='←', font=('Arial', 25), command=self.back_mean)
+		self.back_button.grid(row=4, column=0, stick='wens', padx=1, pady=1)
+
 		#создание кнопок "операции"
 		for d in range(len(self.d_buttons)):
-				self.button = tk.Button(self.root, text=f'{self.d_buttons[d]}', font=('Arial', 15), command=lambda x=d: self.operator_handler(x))
-				self.button.grid(row=d, column=3, stick='wens', padx=5, pady=5)
+				self.button = tk.Button(self.root, text=f'{self.d_buttons[d]}', font=('Arial', 25), command=lambda x=d: self.operator_handler(x))
+				self.button.grid(row=d, column=3, stick='wens', padx=1, pady=1)
 		
-		for x in range(4):
-			self.root.grid_columnconfigure(x, minsize=60)
-			self.root.grid_rowconfigure(x, minsize=60)
+		for x in range(5):
+			self.root.grid_columnconfigure(x, minsize=70)
+			self.root.grid_rowconfigure(x, minsize=70)
 		
 		tk.mainloop()
 			
